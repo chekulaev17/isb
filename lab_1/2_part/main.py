c91f68bb76a11d3f2189dd7d09cafd6cb91ad87e
Dmitry Chekulaev <dimachekulaev17@gmail.com>
2025-04-01 16:23:47 +0400

    change

--- lab_1/2_part/main.py
+++ lab_1/2_part/main.py
@@ -1,59 +1,62 @@
 import json
+from collections import OrderedDict
+from constants import RUSSIAN_FREQ  # Подключаем частоты русского алфавита
+import os  # Для проверки существования файлов
 
-def calculate_frequency(text: str) -> dict:
+# Частоты символов из зашифрованного текста
+ENCRYPTED_FREQ = {
+    'Y': 0.099256, '8': 0.01861, 'S': 0.059553, '-': 0.074442, 't': 0.029777, 'A': 0.119107,
+    '!': 0.016129, 'Q': 0.039702, 'x': 0.038462, '3': 0.021092, 'd': 0.014888, 'G': 0.022333,
+    'R': 0.016129, 'J': 0.07072, '=': 0.011166, 'U': 0.009926, 'I': 0.039702, 'L': 0.062035,
+    'K': 0.027295, 'O': 0.032258, '$': 0.07072, 'Z': 0.011166, 'F': 0.016129, 'E': 0.004963,
+    'n': 0.011166, 'C': 0.007444, 'B': 0.007444, '\n': 0.011166, 'h': 0.012407, 'W': 0.012407,
+    'M': 0.003722, '>': 0.004963, ' ': 0.002481, '9': 0.001241
+}
+
+# 1. Сортируем символы по убыванию частоты
+sorted_encrypted_freq = OrderedDict(sorted(ENCRYPTED_FREQ.items(), key=lambda x: x[1], reverse=True))
+sorted_russian_freq = OrderedDict(sorted(RUSSIAN_FREQ.items(), key=lambda x: x[1], reverse=True))
+
+# 2. Создаём словарь соответствия (замены символов)
+decryption_key = {}
+for (enc_char, _), (rus_char, _) in zip(sorted_encrypted_freq.items(), sorted_russian_freq.items()):
+    decryption_key[enc_char] = rus_char
+
+# 3. Функция дешифровки текста
+def decrypt_text(encrypted_text: str, key: dict) -> str:
     """
-    Подсчитывает частоту каждого символа в тексте.
-    :param text: Входной текст.
-    :return: Словарь с символами и их частотой.
+    Расшифровывает текст, заменяя символы по частотному анализу.
+    :param encrypted_text: зашифрованный текст.
+    :param key: словарь соответствий символов.
+    :return: расшифрованный текст.
     """
-    freq = {}
-    total_symbols = len(text)
+    return ''.join([key.get(char, char) for char in encrypted_text])
 
-    # Подсчёт количества каждого символа
-    for char in text:
-        if char in freq:
-            freq[char] += 1
-        else:
-            freq[char] = 1
+# 4. Чтение зашифрованного текста (читаем файл cod19.txt)
+with open('cod19.txt', 'r', encoding='utf-8') as file:
+    encrypted_text = file.read()
 
-    # Преобразуем количество символов в частоту
-    for char in freq:
-        freq[char] = round(freq[char] / total_symbols, 6)
+# 5. Дешифруем текст
+decrypted_text = decrypt_text(encrypted_text, decryption_key)
 
-    return freq
+# 6. Проверяем и создаем файлы для дешифрованного текста и ключа
+decrypted_text_path = 'decrypted_text.txt'
+key_path = 'key.json'
 
-def save_frequency_to_json(filename: str, freq_dict: dict) -> None:
-    """
-    Сохраняет словарь частот в JSON файл.
-    :param filename: Имя файла для сохранения.
-    :param freq_dict: Словарь с частотами символов.
-    """
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(freq_dict, f, ensure_ascii=False)
+# Если файл с дешифрованным текстом не существует, создаем его
+if not os.path.exists(decrypted_text_path):
+    with open(decrypted_text_path, 'w', encoding='utf-8') as file:
+        file.write(decrypted_text)
 
-def load_frequency_from_json(filename: str) -> dict:
-    """
-    Загружает словарь частот из JSON файла.
-    :param filename: Имя файла для загрузки.
-    :return: Словарь с частотами символов.
-    """
-    with open(filename, 'r', encoding='utf-8') as f:
-        return json.load(f)
+# Если файл с ключом не существует, создаем его
+if not os.path.exists(key_path):
+    with open(key_path, 'w', encoding='utf-8') as file:
+        json.dump(decryption_key, file, ensure_ascii=False, indent=4)
 
-
-# Пример зашифрованного текста (в вашем случае замените это на ваш текст)
-encrypted_text = '''Y8S-tA-!AYQSYxS3dAGYSRJ-=A-SUYItLJ-K-ARxOR$JQRALOZYIJtAJYA$QJFAGYQO$8YxLJ$OFSYQJFA8$=QJx-=A8ORAI$E$SRA!L8LnCUU$KJ-xS3$ALOZYI-Jt3AGYtYZLBJAYGJ-t-!-IYxLJFAILhYJWAGIYZILttA-AQQJ$tAnJYAYQYh$SSYALKJWLOFSYAxACGYdWAhYOFEdA8LSS3d
-GIYZILtt-IYxLS-$AxAQxYBAYn$I$8FAGY!xYOR$JAQY!8LxLJFAGI-OYM$S-RAAQ-QJ$t3AKYJYI3$ALxJYtLJ-!-IWBJAIWJ-SS3$A!L8Ln-AYh$QG$nxLRAW8YhQJxYA-AQKYIYQJFAx3GYOS$S-RAYG$IL>-=QAIL!x-J-$tAJ$dSYOYZ-=AJLK-dAKLKA-QKWQQJx$SS3=ASJ$OO$KJA-AtLE-SSY$AYhWn$S-$A-SUYItLJ-KLAQJLSYxJQRA$9$AhYO$$AxLMSY=
-CJ-ASLGILxO$S-RAYJKI3xLBJASYx3$AZYI-!YSJ3AxALSLO-!$A8LSS3dAGI$8QKL!LJ$OFSY=ALSLO-J-K$A-ALxJYtLJ-!L>--AGIY>$QQYx
-xLMSYAYJt$J-JFAnJYA-SUYItLJKLAS$AJYOFKYAQGYQYhQJxW$JAIL!x-J-BAJ$dSYOYZ-=ASYAAUYIt-IW$JASYx3$AGY8dY83AKAI$E$S-BAQY>-
-LOFS3dACKYSYt-n$QK-dA-ACKYOYZ-n$QKdAGIYhO$tA8$OLRAt-IAhYO$$A-ACUU$KJ-xS3t'''
-
-# Подсчёт частот символов в тексте
-freq_dict = calculate_frequency(encrypted_text)
-
-# Сохранение частот в файл
-save_frequency_to_json('encrypted_freq.json', freq_dict)
-
-# Печать результата
-print("Частотный анализ текста:")
-print(freq_dict)
+# 7. Выводим результат
+print("Исходный текст (зашифрованный):")
+print(encrypted_text[:500])  # Выводим первые 500 символов
+print("\nДешифрованный текст:")
+print(decrypted_text[:500])  # Выводим первые 500 символов
+print("\nНайденный ключ соответствий символов:")
+print(decryption_key)
