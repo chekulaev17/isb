278a7e660d7264d68dad10f48ac2323902464ee5
Dmitry Chekulaev <dimachekulaev17@gmail.com>
2025-04-01 12:46:18 +0400

    add file

--- lab_1/ciph.py
+++ lab_1/ciph.py
@@ -9,3 +9,16 @@ def encrypt_letter(text_letter: str, key_letter: str) -> str:
         key_index = ALPHABET.index(key_letter)
         return ALPHABET[(text_index + key_index) % ALPHABET_SIZE]
     return text_letter
+
+def decrypt_letter(encrypted_letter: str, key_letter: str) -> str:
+    if encrypted_letter in ALPHABET:
+        enc_index = ALPHABET.index(encrypted_letter)
+        key_index = ALPHABET.index(key_letter)
+        return ALPHABET[(enc_index - key_index) % ALPHABET_SIZE]
+    return encrypted_letter
+
+def trithemius_encrypt(text: str, key: str) -> str:
+    return "".join(encrypt_letter(text[i], get_key_letter(key, i)) for i in range(len(text)))
+
+def trithemius_decrypt(encrypted_text: str, key: str) -> str:
+    return "".join(decrypt_letter(encrypted_text[i], get_key_letter(key, i)) for i in range(len(encrypted_text)))
