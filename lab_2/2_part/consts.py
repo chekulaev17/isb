e9000e902dd72d1a2857774c77b3d0231362b941
Dmitry Chekulaev <dimachekulaev17@gmail.com>
2025-05-02 22:40:24 +0400

    build: add consts

---
+++ lab_2/2_part/consts.py
@@ -0,0 +1,11 @@
+CPP_SEQUENCE_TXT = "1_part/cpp_gener/cpp_seq.txt"
+JAVA_SEQUENCE_TXT = "1_part/Java/java_seq.txt"
+PYTHON_SEQUENCE_TXT = "1_part/python/p_seq.txt"
+
+
+TEST_RESULTS_CPP    = "results/test_results_cpp.txt"
+TEST_RESULTS_JAVA   = "results/test_results_java.txt"
+TEST_RESULTS_PYTHON = "results/test_results_python.txt"
+
+
+PI = [0.2148, 0.3672, 0.2305, 0.1875]
