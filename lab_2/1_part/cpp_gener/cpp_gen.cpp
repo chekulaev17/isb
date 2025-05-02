38fd4473d053f4bafcd0065df29efe8670cd42bc
Dmitry Chekulaev <dimachekulaev17@gmail.com>
2025-05-02 20:55:05 +0400

    1

---
+++ lab_2/1_part/cpp_gener/cpp_gen.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+void writeSequence(const char* filepath, const std::string& data) {
+    std::ofstream output(filepath);
+    if (output.is_open()) {
+        output.write(data.c_str(), data.size());
+        output.close();
+    }
+}
+
+std::string generateBinarySequence(std::size_t length) {
+    std::string result;
+    result.reserve(length);
+
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    for (std::size_t i = 0; i < length; i++) {
+        result.push_back((std::rand() % 2) ? '1' : '0');
+    }
+
+    return result;
+}
+
+int main() {
+    constexpr std::size_t sequenceLength = 128;
+    const std::string filename = "cpp_seq.txt";
+
+    auto sequence = generateBinarySequence(sequenceLength);
+    writeSequence(filename.c_str(), sequence);
+
+    return 0;
+}
