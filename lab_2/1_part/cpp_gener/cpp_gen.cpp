f5957829327a166a8c9e4f9e21f6f0d4516b67df
Dmitry Chekulaev <dimachekulaev17@gmail.com>
2025-05-02 22:46:12 +0400

    fix: cpp code

--- lab_2/1_part/cpp_gener/cpp_gen.cpp
+++ lab_2/1_part/cpp_gener/cpp_gen.cpp
@@ -15,30 +15,19 @@ void writeSequence(const char* filepath, const std::string& data) {
 std::string generateBinarySequence(std::size_t length) {
     std::string result;
     result.reserve(length);
-
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
-
     for (std::size_t i = 0; i < length; i++) {
         result.push_back((std::rand() % 2) ? '1' : '0');
     }
-
     return result;
 }
 
 int main() {
     constexpr std::size_t sequenceLength = 128;
     const std::string filename = "cpp_seq.txt";
-
     auto sequence = generateBinarySequence(sequenceLength);
-
-    // Выводим последовательность на экран
     std::cout << "Generated binary sequence: " << sequence << std::endl;
-
-    // Записываем последовательность в файл
     writeSequence(filename.c_str(), sequence);
-
-    // Выводим сообщение о завершении
     std::cout << "Random binary sequence generated and saved to " << filename << std::endl;
-
     return 0;
 }
