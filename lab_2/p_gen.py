b85aa355e3f2201a7492b4076a817d941eb089ac
Dmitry Chekulaev <dimachekulaev17@gmail.com>
2025-05-02 20:24:12 +0400

    add sequences files

--- lab_2/p_gen.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-try:
-    seq = format(random.getrandbits(128), '0128b')
-    with open('p_seq.txt', 'w') as f:
-        f.write(seq + '\n')
-except Exception as e:
-    print(f"Error: {e}")
