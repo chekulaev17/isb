74f2912272f1d504289b82af75494c8d879a550f
Dmitry Chekulaev <dimachekulaev17@gmail.com>
2025-05-26 15:44:35 +0400

    add file file_utils.py.py

---
+++ lab_3/file_utils.py
@@ -0,0 +1,15 @@
+import os
+
+class FileUtils:
+    @staticmethod
+    def read_file(file_path: str) -> bytes:
+        """Чтение данных из файла"""
+        with open(file_path, 'rb') as f:
+            return f.read()
+
+    @staticmethod
+    def write_file(file_path: str, data: bytes):
+        """Запись данных в файл"""
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, 'wb') as f:
+            f.write(data)
